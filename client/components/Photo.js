a0dfb164df6f563974824d0fa6c3355cfa5f94dc
Ryuuzaki <rmorales82@outlook.com>
2019-04-24 18:21:12 +0200

    posts reducer and bind to photo button

--- client/components/Photo.js
+++ client/components/Photo.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import { Link } from 'react-router';
 import CSSTransitionGroup from 'react-addons-css-transition-group';
 
@@ -7,34 +7,39 @@ const Photo = React.createClass({
     const { post, i, comments } = this.props;
     return (
       <figure className="grid-figure">
-          <div className="grid-photo-wrap">
-            <Link to={`/view/${post.code}`}>
-              <img src={post.display_src} alt={post.caption} className="grid-photo" />
+        <div className="grid-photo-wrap">
+          <Link to={`/view/${post.code}`}>
+            <img
+              src={post.display_src}
+              alt={post.caption}
+              className="grid-photo"
+            />
+          </Link>
+          <CSSTransitionGroup
+            transitionName="like"
+            transitionEnterTimeout={500}
+            transitionLeaveTimeout={500}
+          >
+            <span key={post.likes} className="likes-heart">
+              {post.likes}
+            </span>
+          </CSSTransitionGroup>
+        </div>
+        <figCaption>
+          <p>{post.caption}</p>
+          <div className="control-buttons">
+            <button onClick={this.props.increment.bind(null,i)} className="likes">&hearts; {post.likes}</button>
+            <Link className="button" to={`/view/${post.code}`}>
+              <span className="comment-count">
+                <span className="speech-bubble" />&nbsp;
+                {comments[post.code] ? comments[post.code].length : 0}
+              </span>
             </Link>
-            <CSSTransitionGroup transitionName="like"
-              transitionEnterTimeout={500}
-              transitionLeaveTimeout={500}>
-              <span key={post.likes} className="likes-heart">{post.likes}</span>
-
-            </CSSTransitionGroup>
           </div>
-          <figCaption>
-            <p>{post.caption}</p>
-            <div className="control-buttons">
-              <button className="likes">&hearts; {post.likes}</button>
-              <Link className="button" to={`/view/${post.code}`}>
-                <span className="comment-count">
-                  <span className="speech-bubble"></span>&nbsp;
-                  {comments[post.code] ? comments[post.code].length : 0 }
-
-                </span>
-              </Link>
-            </div>
-          </figCaption>
-
+        </figCaption>
       </figure>
-    )
+    );
   }
-})
+});
 
 export default Photo;
--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -1,6 +1,17 @@
-function posts ( state= [], action ){
+function posts ( state=[], action ){
   console.log(state, action);
-  return state;
+  const i = action.index;
+  switch (action.type){
+    case 'INCREMENT_LIKES':
+      //return new state, pure function
+      return [
+        ...state.slice(0,i), // elements before i
+        {...state[i], likes: state[i].likes +1},
+        ...state.slice(i+1), // elements after i
+      ]
+    default:
+      return state;
+  }
 
 }
 
