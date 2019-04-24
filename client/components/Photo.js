d1661d276ef679c196717191bd69e30d57b8f220
Ryuuzaki <rmorales82@outlook.com>
2019-04-24 14:05:47 +0200

    Photo

--- client/components/Photo.js
+++ client/components/Photo.js
@@ -24,7 +24,7 @@ const Photo = React.createClass({
               <button className="likes">&hearts; {post.likes}</button>
               <Link className="button" to={`/view/${post.code}`}>
                 <span className="comment-count">
-                  <span className="speech-bubble"></span>
+                  <span className="speech-bubble"></span>&nbsp;
                   {comments[post.code] ? comments[post.code].length : 0 }
 
                 </span>
