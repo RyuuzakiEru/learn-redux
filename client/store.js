7d164ecb07ec9efc41377ee527ef66a4a5df8148
Ryuuzaki <rmorales82@outlook.com>
2019-04-25 09:42:03 +0200

    Add redux devtools

--- client/store.js
+++ client/store.js
@@ -1,6 +1,6 @@
-import { createStore, compse } from 'redux'
-import { syncHistoryWithStore } from 'react-router-redux'
-import { browserHistory } from 'react-router'
+import { createStore, compose } from 'redux';
+import { syncHistoryWithStore } from 'react-router-redux';
+import { browserHistory } from 'react-router';
 
 // import root reducer
 
@@ -13,15 +13,19 @@ import { __esModule } from 'react-router-redux/lib/reducer';
 const defaultState = {
   posts,
   comments
-}
+};
 
-const store = createStore(rootReducer, defaultState);
+const store = createStore(
+  rootReducer,
+  defaultState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 export const history = syncHistoryWithStore(browserHistory, store);
 
 if (module.hot) {
   module.hot.accept('./reducers', () => {
-    const nextRootReducer = require ('./reducers/index').default;
+    const nextRootReducer = require('./reducers/index').default;
     store.replaceReducer(nextRootReducer);
   });
 }
